05e1f7ab376e27d70c2a90aedf26cb0a2479ae6c
Eugeny Tatarincev <et@virtoway.com>
2017-11-09 12:10:18 +0200

    Load a user-defined  settings from the special section.

--- VirtoCommerce.Storefront/Infrastructure/StorefrontOptions.cs
+++ VirtoCommerce.Storefront/Infrastructure/StorefrontOptions.cs
@@ -8,7 +8,6 @@ namespace VirtoCommerce.Storefront.Infrastructure
     {
         public string DefaultStore { get; set; }
         public TimeSpan ChangesPoolingInterval { get; set; } = TimeSpan.FromMinutes(1);
-        public Dictionary<string, object> Settings { get; set; } = new Dictionary<string, object>();
         public PlatformEndpointOptions Endpoint { get; set; }
         public LiquidThemeEngineOptions LiquidThemeEngine { get; set; }
         public RequireHttpsOptions RequireHttps { get; set; }
