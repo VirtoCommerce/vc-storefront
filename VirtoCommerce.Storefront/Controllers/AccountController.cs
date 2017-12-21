829b983b1b3f4130f6825494a3018ba00852b040
Valeriy Krashenko <vk@virtoway.com>
2017-12-21 17:48:52 +0200

    refactor external logins assotiation method

--- VirtoCommerce.Storefront/Controllers/AccountController.cs
+++ VirtoCommerce.Storefront/Controllers/AccountController.cs
@@ -209,10 +209,11 @@ namespace VirtoCommerce.Storefront.Controllers
                     return View("lockedout", WorkContext);
                 }
 
+                IdentityResult identityResult;
+
                 if (currentUser.IsRegisteredUser == true)
                 {
-                    currentUser.ExternalLogins.Add(new ExternalUserLoginInfo { ProviderKey = loginInfo.ProviderKey, ProviderDisplayName = loginInfo.ProviderDisplayName ,LoginProvider = loginInfo.LoginProvider });
-                    var identityResult = await _signInManager.UserManager.UpdateAsync(currentUser);
+                    identityResult = await _signInManager.UserManager.AddLoginAsync(currentUser, loginInfo);
                 }
                 else
                 {
@@ -223,15 +224,16 @@ namespace VirtoCommerce.Storefront.Controllers
                         StoreId = WorkContext.CurrentStore.Id,
                     };
                     user.ExternalLogins.Add(new ExternalUserLoginInfo { ProviderKey = loginInfo.ProviderKey, LoginProvider = loginInfo.LoginProvider });
-                    var identityResult = await _signInManager.UserManager.CreateAsync(user);
-                    if (!identityResult.Succeeded)
+                    identityResult = await _signInManager.UserManager.CreateAsync(user);
+                }
+
+                if (!identityResult.Succeeded)
+                {
+                    foreach (var error in identityResult.Errors)
                     {
-                        foreach (var error in identityResult.Errors)
-                        {
-                            ModelState.AddModelError(string.Empty, error.Description);
-                        }
-                        return View("customers/login", WorkContext);
+                        ModelState.AddModelError(string.Empty, error.Description);
                     }
+                    return View("customers/login", WorkContext);
                 }
             }
 
