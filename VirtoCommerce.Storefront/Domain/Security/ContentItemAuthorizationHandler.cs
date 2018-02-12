f76dc00c80e7c18be81e51067586ef3d1f21dbb6
OlegoO <zhukoo@gmail.com>
2018-02-12 07:58:33 +0200

    #70 Limits anonymous access for CMS pages (#71)

---
+++ VirtoCommerce.Storefront/Domain/Security/ContentItemAuthorizationHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using VirtoCommerce.Storefront.Model.StaticContent;
+
+namespace VirtoCommerce.Storefront.Domain.Security
+{
+    public class ContentItemAuthorizeRequirement : IAuthorizationRequirement { }
+
+    public class ContentItemAuthorizationHandler : AuthorizationHandler<ContentItemAuthorizeRequirement, ContentItem>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ContentItemAuthorizeRequirement requirement, ContentItem resource)
+        {
+            if (resource.Authorize)
+            {
+                if(context.User.Identity.IsAuthenticated)
+                    context.Succeed(requirement);
+            }
+            else 
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+    }
+}
