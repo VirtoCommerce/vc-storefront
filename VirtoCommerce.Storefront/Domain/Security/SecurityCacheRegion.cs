a2c9025e60e67e33b9ba2135253ae214eba20191
Eugeny Tatarincev <et@virtoway.com>
2018-02-14 11:18:39 +0200

    Added user eviction from cache when external logins added https://github.com/VirtoCommerce/vc-storefront-core/issues/73

--- VirtoCommerce.Storefront/Domain/Security/SecurityCacheRegion.cs
+++ VirtoCommerce.Storefront/Domain/Security/SecurityCacheRegion.cs
@@ -1,8 +1,32 @@
-﻿using VirtoCommerce.Storefront.Model.Common.Caching;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using Microsoft.Extensions.Primitives;
+using VirtoCommerce.Storefront.Model.Common.Caching;
 
 namespace VirtoCommerce.Storefront.Domain.Security
 {
     public class SecurityCacheRegion : CancellableCacheRegion<SecurityCacheRegion>
-    {  
+    {
+        private static readonly ConcurrentDictionary<string, CancellationTokenSource> _securityCacheRegionTokenLookup =
+         new ConcurrentDictionary<string, CancellationTokenSource>();
+
+        public static IChangeToken CreateChangeToken(string userId)
+        {
+            if (userId == null)
+            {
+                throw new ArgumentNullException(nameof(userId));
+            }
+            var cancellationTokenSource = _securityCacheRegionTokenLookup.GetOrAdd(userId, new CancellationTokenSource());
+            return new CompositeChangeToken(new[] { CreateChangeToken(), new CancellationChangeToken(cancellationTokenSource.Token) });
+        }
+
+        public static void ExpireUser(string userId)
+        {
+            if (_securityCacheRegionTokenLookup.TryRemove(userId, out var token))
+            {
+                token.Cancel();
+            }
+        }
     }
 }
