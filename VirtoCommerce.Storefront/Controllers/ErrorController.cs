e63e4813c845d5e08c2e2cd85bdfd582964eab31
Aleksandr Vishnyakov <asvishnyakov@hotmail.com>
2018-10-19 11:01:51 +0500

    Fixed routing in error controller (#169)

--- VirtoCommerce.Storefront/Controllers/ErrorController.cs
+++ VirtoCommerce.Storefront/Controllers/ErrorController.cs
@@ -9,7 +9,7 @@ namespace VirtoCommerce.Storefront.Controllers
     [StorefrontRoute("error")]
     public class ErrorController : Controller
     {
-        [HttpGet("{errCode}")]
+        [Route("{errCode}")]
         public IActionResult Error(int? errCode)
         {
             var exceptionFeature = base.HttpContext.Features.Get<IExceptionHandlerFeature>();
@@ -24,7 +24,7 @@ namespace VirtoCommerce.Storefront.Controllers
             return View();
         }
 
-        [HttpGet("AccessDenied")]
+        [Route("AccessDenied")]
         public IActionResult AccessDenied()
         {          
             return View("AccessDenied");
