c4f3500d418d66cd31cca159b0f71aab8458a77f
Andrey A. Vasenev <avas.the.wise@gmail.com>
2019-01-23 21:17:03 +0700

    Fix for Money.TruncatedAmount behavior (#192)
    * Added some tests for money properties.
    
    * Fixed an implementation of Money.TruncatedAmount to match description in the comment and to pass tests.
    
    * Fixed a build error of the VirtoCommerce.Storefront.Tests project.
    
    * Added a CI trait for the MoneyOperationTests class.

---
+++ VirtoCommerce.Storefront.Tests/Model/MoneyOperationTests.cs
@@ -0,0 +1,41 @@
+using VirtoCommerce.Storefront.Model.Common;
+using Xunit;
+using StorefrontLanguage = VirtoCommerce.Storefront.Model.Language;
+
+namespace VirtoCommerce.Storefront.Tests.Model
+{
+    [Trait("Category", "CI")]
+    public class MoneyOperationTests
+    {
+        private static readonly StorefrontLanguage TestLanguage = new StorefrontLanguage("en-US");
+        private static readonly Currency TestCurrency = new Currency(TestLanguage, "TPT", "Test points", "T", 1.0m);
+
+        [Theory]
+        [InlineData(  0.0,     0.0,    0.0,  "T0.00",   "T0",   "0")]
+        [InlineData(  0.99,    0.99,   0.99, "T0.99",   "T1",   "1")]
+        [InlineData(  0.999,   1.00,   0.99, "T1.00",   "T1",   "1")]
+        [InlineData(  1.445,   1.45,   1.44, "T1.45",   "T1",   "1")]
+        [InlineData(  1.455,   1.46,   1.45, "T1.46",   "T1",   "1")]
+        [InlineData( -1.445,  -1.45,  -1.44, "(T1.45)", "(T1)", "(1)")]
+        [InlineData( -1.455,  -1.46,  -1.45, "(T1.46)", "(T1)", "(1)")]
+        [InlineData( 12.34,   12.34,  12.34, "T12.34",  "T12",  "12")]
+        [InlineData( 12.345,  12.35,  12.34, "T12.35",  "T12",  "12")]
+        [InlineData(123.456, 123.46, 123.45, "T123.46", "T123", "123")]
+        [InlineData(123.5,   123.5,  123.5,  "T123.50", "T124", "124")]
+        public void TestGettingMoneyProperties(decimal internalAmount, decimal expectedAmount,
+            decimal expectedTruncatedAmount, string expectedFormattedAmount, string expectedFormattedAmountWithoutPoint,
+            string expectedFormattedAmountWithoutPointAndCurrency)
+        {
+            // Arrange
+            var money = new Money(internalAmount, TestCurrency);
+
+            // Act & Assert
+            Assert.Equal(internalAmount, money.InternalAmount);
+            Assert.Equal(expectedAmount, money.Amount);
+            Assert.Equal(expectedTruncatedAmount, money.TruncatedAmount);
+            Assert.Equal(expectedFormattedAmount, money.FormattedAmount);
+            Assert.Equal(expectedFormattedAmountWithoutPoint, money.FormattedAmountWithoutPoint);
+            Assert.Equal(expectedFormattedAmountWithoutPointAndCurrency, money.FormattedAmountWithoutPointAndCurrency);
+        }
+    }
+}
--- VirtoCommerce.Storefront.Tests/ResponseCaching/ResponseCachingTests.cs
+++ VirtoCommerce.Storefront.Tests/ResponseCaching/ResponseCachingTests.cs
@@ -11,6 +11,7 @@ using Moq;
 using VirtoCommerce.LiquidThemeEngine;
 using VirtoCommerce.Storefront.Model.Stores;
 using Xunit;
+using Language = VirtoCommerce.Storefront.Model.Language;
 
 namespace VirtoCommerce.Storefront.Tests.OutputCache
 {
@@ -30,7 +31,7 @@ namespace VirtoCommerce.Storefront.Tests.OutputCache
                         mockEngine.Setup(x => x.ResolveTemplatePath(It.IsIn(new[] { "index" }))).Returns("path");
                         services.AddSingleton(mockEngine.Object);
                         var mockStoreService = new Mock<IStoreService>();
-                        mockStoreService.Setup(x => x.GetAllStoresAsync()).ReturnsAsync(new Store[] { new Store { Id = "Electronics", DefaultLanguage = new Model.Language("en-US") } });
+                        mockStoreService.Setup(x => x.GetAllStoresAsync()).ReturnsAsync(new Store[] { new Store { Id = "Electronics", DefaultLanguage = new Language("en-US") } });
                         services.AddSingleton(mockStoreService.Object);
 
                     })
