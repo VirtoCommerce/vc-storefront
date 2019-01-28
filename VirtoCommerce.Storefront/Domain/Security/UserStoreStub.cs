e6e9fcb0340d49fcbb45a1bf4e26c238ec2e2412
Eugeny Tatarincev <et@virtoway.com>
2019-01-28 10:12:27 +0200

    Inherits UserStoreStub form two new interfaces IUserPhoneNumberStore and IUserTwoFactorStore

--- VirtoCommerce.Storefront/Domain/Security/UserStoreStub.cs
+++ VirtoCommerce.Storefront/Domain/Security/UserStoreStub.cs
@@ -19,7 +19,7 @@ namespace VirtoCommerce.Storefront.Domain.Security
 {
     //Stub for UserManager
     public sealed class UserStoreStub : IUserStore<User>, IUserEmailStore<User>, IUserPasswordStore<User>, IUserLockoutStore<User>, IUserLoginStore<User>,
-        IUserSecurityStampStore<User>, IUserClaimStore<User>, IRoleStore<Role>
+        IUserSecurityStampStore<User>, IUserClaimStore<User>, IRoleStore<Role>, IUserPhoneNumberStore<User>, IUserTwoFactorStore<User>
     {
         private readonly ISecurity _platformSecurityApi;
         private readonly IStorefrontMemoryCache _memoryCache;
@@ -370,6 +370,44 @@ namespace VirtoCommerce.Storefront.Domain.Security
         {
             IList<User> result = new List<User>();
             return Task.FromResult(result);
+        }
+        #endregion
+
+        #region IUserPhoneNumberStore<User> members
+        public Task SetPhoneNumberAsync(User user, string phoneNumber, CancellationToken cancellationToken)
+        {
+            user.PhoneNumber = phoneNumber;
+            return Task.CompletedTask;
+        }
+
+        public Task<string> GetPhoneNumberAsync(User user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.PhoneNumber);
+        }
+
+        public Task<bool> GetPhoneNumberConfirmedAsync(User user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.PhoneNumberConfirmed);
+        }
+
+        public Task SetPhoneNumberConfirmedAsync(User user, bool confirmed, CancellationToken cancellationToken)
+        {
+            user.PhoneNumberConfirmed = confirmed;
+            return Task.CompletedTask;
+        }
+
+        #endregion
+
+        #region IUserTwoFactorStore<User> members
+        public Task SetTwoFactorEnabledAsync(User user, bool enabled, CancellationToken cancellationToken)
+        {
+            user.TwoFactorEnabled = enabled;
+            return Task.CompletedTask;
+        }
+
+        public Task<bool> GetTwoFactorEnabledAsync(User user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.TwoFactorEnabled);
         }
         #endregion
 
