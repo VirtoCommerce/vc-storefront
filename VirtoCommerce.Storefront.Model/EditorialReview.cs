5b4d29a676a1cd0816772a3c863b873a5317b061
Eugeny Tatarincev <et@virtoway.com>
2019-02-20 20:13:18 +0200

    Extend EditorialReview with Content property alias

--- VirtoCommerce.Storefront.Model/EditorialReview.cs
+++ VirtoCommerce.Storefront.Model/EditorialReview.cs
@@ -5,6 +5,7 @@ namespace VirtoCommerce.Storefront.Model
     public partial class EditorialReview : LocalizedString, IAccessibleByIndexKey
     {
         public string ReviewType { get; set; }
+        public string Content => Value;
 
         public string IndexKey => ReviewType;
     }
