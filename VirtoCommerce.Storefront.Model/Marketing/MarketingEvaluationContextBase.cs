3d864ff44f6fc0e193f48dceba68fef7d2d08c6f
Eugeny Tatarincev <et@virtoway.com>
2019-03-08 10:37:50 +0200

    remove userId from cache key for pricing and marketing evaluation

--- VirtoCommerce.Storefront.Model/Marketing/MarketingEvaluationContextBase.cs
+++ VirtoCommerce.Storefront.Model/Marketing/MarketingEvaluationContextBase.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using VirtoCommerce.Storefront.Model.Common;
 using VirtoCommerce.Storefront.Model.Security;
@@ -25,7 +26,10 @@ namespace VirtoCommerce.Storefront.Model.Marketing
             yield return StoreId;
             yield return Language;
             yield return Currency;
-            yield return User;
+            //Remove user for equality because marketing promotions very rarely depend on concrete customer and exclude  user from  cache key can have significant affect to performance
+            //yield return User;
+            yield return string.Join('&', User?.Contact?.UserGroups ?? Array.Empty<string>());
+            yield return string.Join('&', User?.Contact?.OrganizationsIds ?? Array.Empty<string>());
         }
 
     }
--- VirtoCommerce.Storefront.Model/Pricing/PriceEvaluationContext.cs
+++ VirtoCommerce.Storefront.Model/Pricing/PriceEvaluationContext.cs
@@ -1,12 +1,11 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
 using VirtoCommerce.Storefront.Model.Common;
 
 namespace VirtoCommerce.Storefront.Model.Pricing
 {
     public class PriceEvaluationContext : ValueObject
-    {       
+    {
         public string StoreId { get; set; }
         public string CatalogId { get; set; }
         public IList<string> ProductIds { get; set; } = new List<string>();
@@ -18,43 +17,30 @@ namespace VirtoCommerce.Storefront.Model.Pricing
         public string OrganizationId { get; set; }
 
         public string Currency { get; set; }
-
-        public object ContextObject { get; set; }
-
         public string GeoCity { get; set; }
-
-        public string GeoState { get; set; }
-
         public string GeoCountry { get; set; }
-
-        public string GeoContinent { get; set; }
-
+        public string GeoState { get; set; }
         public string GeoZipCode { get; set; }
-
-        public string GeoConnectionType { get; set; }
-
         public string GeoTimeZone { get; set; }
-        
-        public string GeoIpRoutingType { get; set; }
-        
-        public string GeoIspSecondLevel { get; set; }
-        
-        public string GeoIspTopLevel { get; set; }
-        
-        public int? ShopperAge { get; set; }
-        
-        public string ShopperGender { get; set; }
-        
         public string Language { get; set; }
 
         public IList<string> UserGroups { get; set; } = new List<string>();
 
-        public string ShopperSearchedPhraseInStore { get; set; }
-        
-        public string ShopperSearchedPhraseOnInternet { get; set; }
-        
-        public string CurrentUrl { get; set; }
-        
-        public string ReferredUrl { get; set; }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return CatalogId;
+            yield return Currency;
+            yield return StoreId;
+            yield return OrganizationId;
+            yield return Language;
+            yield return Quantity;
+
+            //Remove user for equality because marketing promotions very rarely depend on concrete customer and exclude  user from  cache key can have significant affect to performance
+            //yield return CustomerId;
+            yield return string.Join('&', ProductIds ?? Array.Empty<string>());
+            yield return string.Join('&', PricelistIds ?? Array.Empty<string>());
+            yield return string.Join('&', UserGroups ?? Array.Empty<string>());
+        }
     }
 }
