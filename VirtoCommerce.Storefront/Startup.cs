110e391ff608191c5053a31e1cfa92caeee70793
alexandr andreev <alivemenone@gmail.com>
2019-05-12 22:42:01 +0300

    fix  CompatibilityVersion.Version_2_2 routing error (#256)
    * do not use Endpoint Routing logic
    
    * remove version for Microsoft.NETCore.App packages
    
    * add missed space

--- VirtoCommerce.Storefront/Startup.cs
+++ VirtoCommerce.Storefront/Startup.cs
@@ -272,6 +272,9 @@ namespace VirtoCommerce.Storefront
 
                 // To include only Api controllers to swagger document
                 options.Conventions.Add(new ApiExplorerApiControllersConvention());
+
+                // Use the routing logic of ASP.NET Core 2.1 or earlier:
+                options.EnableEndpointRouting = false;
             }).AddJsonOptions(options =>
             {
                 options.SerializerSettings.DefaultValueHandling = DefaultValueHandling.Include;
@@ -290,7 +293,7 @@ namespace VirtoCommerce.Storefront
             {
                 options.ViewEngines.Add(snapshotProvider.GetService<ILiquidViewEngine>());
             })
-            .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
 
             //Register event handlers via reflection
