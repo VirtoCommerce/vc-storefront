c0bfbbfb2be74ebc8e0c5d731de82bf68aa7eb29
trueboroda <superiorboroda@gmail.com>
2019-10-02 19:03:55 +0300

    fix wrong redirect to login page when current store is not default

--- VirtoCommerce.Storefront/Domain/Security/CustomCookieAuthenticationEvents.cs
+++ VirtoCommerce.Storefront/Domain/Security/CustomCookieAuthenticationEvents.cs
@@ -1,13 +1,20 @@
+using System;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using VirtoCommerce.Storefront.Extensions;
+using VirtoCommerce.Storefront.Model.Common;
 
 namespace VirtoCommerce.Storefront.Domain.Security
 {
     public class CustomCookieAuthenticationEvents : CookieAuthenticationEvents
     {
+        private readonly IStorefrontUrlBuilder _storefrontUrlBuilder;
+        public CustomCookieAuthenticationEvents(IStorefrontUrlBuilder storefrontUrlBuilder)
+        {
+            _storefrontUrlBuilder = storefrontUrlBuilder;
+        }
         public override Task RedirectToLogin(RedirectContext<CookieAuthenticationOptions> context)
         {
             if (context.Request.Path.IsApi())
@@ -15,6 +22,7 @@ namespace VirtoCommerce.Storefront.Domain.Security
                 context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
                 return Task.CompletedTask;
             }
+            context.RedirectUri = AdjustRedirectUri(context.RedirectUri);
             return base.RedirectToLogin(context);
         }
 
@@ -25,8 +33,24 @@ namespace VirtoCommerce.Storefront.Domain.Security
                 context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
                 return Task.CompletedTask;
             }
+            context.RedirectUri = AdjustRedirectUri(context.RedirectUri);
             return base.RedirectToAccessDenied(context);
         }
-        
+
+        /// <summary>
+        /// add store and lng segments of current store to uri
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <returns></returns>
+        private string AdjustRedirectUri(string uri)
+        {
+            var redirectUri = new Uri(uri);
+            var adjustedAbsolutePath = _storefrontUrlBuilder.ToAppAbsolute(redirectUri.AbsolutePath);
+            var builder = new UriBuilder(redirectUri);
+            builder.Path = adjustedAbsolutePath;
+            string result = builder.Uri.ToString();
+            return result;
+        }
+
     }
 }
