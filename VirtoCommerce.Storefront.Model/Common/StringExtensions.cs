ed502cca3b3741a35a7f4ffb1a0568ed608e4898
Anton <superiorboroda@gmail.com>
2019-10-07 20:50:56 +0300

    Fix handelize filter. Any lng chars are allowing (#306)
    * allow any lng chars in handelize filter
    
    * don't do revomeAccent. allow any lng chars without pinctuation

--- VirtoCommerce.Storefront.Model/Common/StringExtensions.cs
+++ VirtoCommerce.Storefront.Model/Common/StringExtensions.cs
@@ -14,7 +14,7 @@ namespace VirtoCommerce.Storefront.Model.Common
         private static readonly Regex _regex1 = new Regex(@"([A-Z]+)([A-Z][a-z])", RegexOptions.Compiled);
         private static readonly Regex _regex2 = new Regex(@"([a-z\d])([A-Z])", RegexOptions.Compiled);
         private static readonly Regex _emailRegex = new Regex(@"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-||_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+([a-z]+|\d|-|\.{0,1}|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])?([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$", RegexOptions.IgnoreCase, TimeSpan.FromSeconds(1));
-        private static  char[] _maskChars = { '*', '?' };
+        private static char[] _maskChars = { '*', '?' };
         public static string PascalToKebabCase(this string name)
         {
             if (name == null)
@@ -122,9 +122,9 @@ namespace VirtoCommerce.Storefront.Model.Common
             var retVal = phrase;
             if (phrase != null)
             {
-                retVal = phrase.RemoveAccent().ToLower();
+                retVal = phrase.ToLower();
 
-                retVal = Regex.Replace(retVal, @"[^a-z0-9\s-]", ""); // invalid chars           
+                retVal = Regex.Replace(retVal, @"[^\p{L}\d\s-]", ""); // invalid chars           
                 retVal = Regex.Replace(retVal, @"\s+", " ").Trim(); // convert multiple spaces into one space   
                 retVal = retVal.Substring(0, retVal.Length <= 240 ? retVal.Length : 240).Trim(); // cut and trim it   
                 retVal = Regex.Replace(retVal, @"\s", "-"); // hyphens   
