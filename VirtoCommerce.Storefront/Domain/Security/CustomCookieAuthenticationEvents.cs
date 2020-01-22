3192e49a0fb609937797bd9cdfd3038e1c58899d
Andrey Kozlov <yecli.github@gmail.com>
2020-01-22 13:49:42 +0500

    VP-1249: Fix typo

--- VirtoCommerce.Storefront/Domain/Security/CustomCookieAuthenticationEvents.cs
+++ VirtoCommerce.Storefront/Domain/Security/CustomCookieAuthenticationEvents.cs
@@ -8,58 +8,58 @@ using VirtoCommerce.Storefront.Model.Common;
 
 namespace VirtoCommerce.Storefront.Domain.Security
 {
-	public class CustomCookieAuthenticationEvents : CookieAuthenticationEvents
-	{
-		private readonly IStorefrontUrlBuilder _storefrontUrlBuilder;
+    public class CustomCookieAuthenticationEvents : CookieAuthenticationEvents
+    {
+        private readonly IStorefrontUrlBuilder _storefrontUrlBuilder;
 
-		public CustomCookieAuthenticationEvents(IStorefrontUrlBuilder storefrontUrlBuilder)
-		{
-			_storefrontUrlBuilder = storefrontUrlBuilder;
-		}
+        public CustomCookieAuthenticationEvents(IStorefrontUrlBuilder storefrontUrlBuilder)
+        {
+            _storefrontUrlBuilder = storefrontUrlBuilder;
+        }
 
-		public override Task RedirectToLogin(RedirectContext<CookieAuthenticationOptions> context)
-		{
-			if (context.Request.Path.IsApi())
-			{
-				context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-				return Task.CompletedTask;
-			}
+        public override Task RedirectToLogin(RedirectContext<CookieAuthenticationOptions> context)
+        {
+            if (context.Request.Path.IsApi())
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                return Task.CompletedTask;
+            }
 
-			context.RedirectUri = GetStoreAbsoluteUri(context.RedirectUri);
+            context.RedirectUri = GetStoreAbsoluteUri(context.RedirectUri);
 
-			return base.RedirectToLogin(context);
-		}
+            return base.RedirectToLogin(context);
+        }
 
-		public override Task RedirectToAccessDenied(RedirectContext<CookieAuthenticationOptions> context)
-		{
-			if (context.Request.Path.IsApi())
-			{
-				context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
-				return Task.CompletedTask;
-			}
+        public override Task RedirectToAccessDenied(RedirectContext<CookieAuthenticationOptions> context)
+        {
+            if (context.Request.Path.IsApi())
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                return Task.CompletedTask;
+            }
 
-			context.RedirectUri = GetStoreAbsoluteUri(context.RedirectUri);
+            context.RedirectUri = GetStoreAbsoluteUri(context.RedirectUri);
 
-			return base.RedirectToAccessDenied(context);
-		}
+            return base.RedirectToAccessDenied(context);
+        }
 
 
-		private string GetStoreAbsoluteUri(string uri)
-		{
-			//Need to build from an host absolute url a  relative  store-based url
-			// http://localhost/Account/Login -> http://localhost/{store}/{lang}/Account/Login
-			var redirectUri = new UriBuilder(uri);
-			var storeBasedRedirectPath = _storefrontUrlBuilder.ToAppAbsolute(redirectUri.Path);
+        private string GetStoreAbsoluteUri(string uri)
+        {
+            //Need to build from an host absolute url a  relative  store-based url
+            // http://localhost/Account/Login -> http://localhost/{store}/{lang}/Account/Login
+            var redirectUri = new UriBuilder(uri);
+            var storeBasedRedirectPath = _storefrontUrlBuilder.ToAppAbsolute(redirectUri.Path);
 
-			// Checks whether fullRedirectPath is absolute path (starts with scheme), and extract local path if it is
-			if (Uri.TryCreate(storeBasedRedirectPath, UriKind.Absolute, out var absoluteUri))
-			{
-				storeBasedRedirectPath = absoluteUri.AbsolutePath;
-			}
+            // Checks whether path is absolute path (starts with scheme), and extract local path if it is
+            if (Uri.TryCreate(storeBasedRedirectPath, UriKind.Absolute, out var absoluteUri))
+            {
+                storeBasedRedirectPath = absoluteUri.AbsolutePath;
+            }
 
-			redirectUri.Path = storeBasedRedirectPath;
+            redirectUri.Path = storeBasedRedirectPath;
 
-			return redirectUri.Uri.ToString();
-		}
-	}
+            return redirectUri.Uri.ToString();
+        }
+    }
 }
