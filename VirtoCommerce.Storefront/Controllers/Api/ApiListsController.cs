fdfb06b62ab70fcc6ad1cb659c49b9c141d923d6
Ilya <ilya.ivanov6@gmail.com>
2020-04-09 14:36:35 +0200

    Add missing methods for working with lists
    * ChangeListItem for changing quantity
    * ClearList for removing all items from list

--- VirtoCommerce.Storefront/Controllers/Api/ApiListsController.cs
+++ VirtoCommerce.Storefront/Controllers/Api/ApiListsController.cs
@@ -99,6 +99,43 @@ namespace VirtoCommerce.Storefront.Controllers.Api
             }
         }
 
+        // PUT: storefrontapi/lists/{listName}/{type}/items/{lineItemId}?quantity=...
+        [HttpPut("{listName}/{type}/items/{lineItemId}")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ChangeListItem(string listName, string type, string lineItemId, int quantity)
+        {
+            var unescapedListName = Uri.UnescapeDataString(listName);
+            //Need lock to prevent concurrent access to same list
+            using (await AsyncLock.GetLockByKey(GetAsyncListKey(WorkContext, unescapedListName, type)).LockAsync())
+            {
+                var cartBuilder = await LoadOrCreateCartAsync(unescapedListName, type);
+
+                var lineItem = cartBuilder.Cart.Items.FirstOrDefault(i => i.Id == lineItemId);
+                if (lineItem != null)
+                {
+                    await cartBuilder.ChangeItemQuantityAsync(lineItemId, quantity);
+                    await cartBuilder.SaveAsync();
+                }
+            }
+            return Ok();
+        }
+
+        // POST: storefrontapi/lists/{listName}/{type}/clear
+        [HttpPost("{listName}/{type}/clear")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ClearList(string listName, string type)
+        {
+            var unescapedListName = Uri.UnescapeDataString(listName);
+            //Need lock to prevent concurrent access to same list
+            using (await AsyncLock.GetLockByKey(GetAsyncListKey(WorkContext, unescapedListName, type)).LockAsync())
+            {
+                var cartBuilder = await LoadOrCreateCartAsync(unescapedListName, type);
+                await cartBuilder.ClearAsync();
+                await cartBuilder.SaveAsync();
+            }
+            return Ok();
+        }
+
         // POST: storefrontapi/lists/search
         [HttpPost("search")]
         [ValidateAntiForgeryToken]
