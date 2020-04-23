ae13908b81a205bc20b005aa656cd1e68d967fa6
Timofey <tburashov@gmail.com>
2020-04-23 12:25:52 +0700

    VP-1813: improve the FeatureFilter
    add the imitation of the bulk processing

--- VirtoCommerce.LiquidThemeEngine/Filters/FeatureFilter.cs
+++ VirtoCommerce.LiquidThemeEngine/Filters/FeatureFilter.cs
@@ -1,23 +1,65 @@
 namespace VirtoCommerce.LiquidThemeEngine.Filters
 {
+    using System;
+
     using global::Scriban;
+
+    using Newtonsoft.Json.Linq;
 
     public class FeatureFilter
     {
         public static string IsFeatureActive(TemplateContext context, string key, params object[] variables)
         {
-            bool result;
+            if (string.IsNullOrEmpty(key))
+            {
+                return IsFeaturesActive(context, key, variables);
+            }
 
+            string result;
             if (!(context.TemplateLoader is ShopifyLiquidThemeEngine themeEngine))
             {
-                result = false;
+                result = Convert.ToString(false);
             }
             else
             {
-                result = themeEngine.IsFeatureActive(key);
+                result = IsFeatureActive(themeEngine, key);
             }
 
-            return result ? "true" : "false";
+            return result;
+        }
+
+        public static string IsFeaturesActive(TemplateContext context, string key, params object[] variables)
+        {
+            if (!string.IsNullOrEmpty(key))
+            {
+                return IsFeatureActive(context, key, variables);
+            }
+
+            string result;
+            if (!(context.TemplateLoader is ShopifyLiquidThemeEngine themeEngine))
+            {
+                result = Convert.ToString(false);
+            }
+            else
+            {
+                var jObject = new JObject();
+
+                foreach (string featureName in variables)
+                {
+                    var isFeatureActiveResult = IsFeatureActive(themeEngine, featureName);
+                    jObject.Add(featureName, isFeatureActiveResult);
+                }
+
+                result = jObject.ToString();
+            }
+
+            return result;
+        }
+
+        private static string IsFeatureActive(ShopifyLiquidThemeEngine themeEngine, string key)
+        {
+            var isActive = themeEngine.IsFeatureActive(key);
+            return Convert.ToString(isActive);
         }
     }
 }
