3acd7e2bf50e1b6d955de67361a46b5b9d22c78f
Andrei Kolchanov <akak1977@gmail.com>
2020-05-06 17:26:34 +0500

    Review based changes: change settings value types to object

--- VirtoCommerce.Storefront/Domain/Common/SettingConverter.cs
+++ VirtoCommerce.Storefront/Domain/Common/SettingConverter.cs
@@ -8,15 +8,17 @@ namespace VirtoCommerce.Storefront.Domain
     {
         public static SettingEntry ToSettingEntry(this platformDto.ObjectSettingEntry settingDto)
         {
-            var retVal = new SettingEntry();
-            retVal.DefaultValue = settingDto.DefaultValue?.ToString();
-            retVal.IsArray = false;
-            retVal.Name = settingDto.Name;
-            retVal.Value = settingDto.Value?.ToString();
-            retVal.ValueType = settingDto.ValueType;
+            var retVal = new SettingEntry
+            {
+                DefaultValue = settingDto.DefaultValue,
+                IsArray = false,
+                Name = settingDto.Name,
+                Value = settingDto.Value,
+                ValueType = settingDto.ValueType
+            };
             if (settingDto.AllowedValues != null)
             {
-                retVal.AllowedValues = settingDto.AllowedValues.Cast<string>().ToList();
+                retVal.AllowedValues = settingDto.AllowedValues.ToList();
             }
             return retVal;
         }
