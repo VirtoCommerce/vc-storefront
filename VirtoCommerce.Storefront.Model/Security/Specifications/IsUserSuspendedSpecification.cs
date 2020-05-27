be33cf5d58afd45eaee63d5648fecd563b269912
Andrey Kozlov <yecli.github@gmail.com>
2020-05-27 18:31:52 +0500

    VP-2593: Add check for lockOut

--- VirtoCommerce.Storefront.Model/Security/Specifications/IsUserSuspendedSpecification.cs
+++ VirtoCommerce.Storefront.Model/Security/Specifications/IsUserSuspendedSpecification.cs
@@ -2,11 +2,11 @@ using VirtoCommerce.Storefront.Model.Common.Specifications;
 
 namespace VirtoCommerce.Storefront.Model.Security.Specifications
 {
-    public class IsUserSuspendedSpecification: ISpecification<User>
+    public class IsUserSuspendedSpecification : ISpecification<User>
     {
         public bool IsSatisfiedBy(User obj)
         {
-            return obj.UserState == AccountState.Rejected;
+            return obj.UserState == AccountState.Rejected || obj.IsLockedOut;
         }
     }
 }
