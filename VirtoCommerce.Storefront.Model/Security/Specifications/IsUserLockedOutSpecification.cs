VirtoCommerce/vc-storefront

commit 3338df3e7f02d0f811b57efdfb98fb2740ce2b02

VP-2067 Add login option to redirect user to correct store instead of returning error (#409)
* Add login option to redirect user to correct store instead of returning error

* Refactoring

* Remove unnecessary sign out

* Revert "Remove unnecessary sign out"

This reverts commit 29d9e05f7db64675e3059bc2eb02fadddf1e1552.

* Fix issues

* Revert back change in stre work context builder middleware

Co-authored-by: Burashov Timofey <tburashov@gmail.com>

VirtoCommerce.Storefront/Controllers/AccountController.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
@@ -26,6 +27,7 @@ namespace VirtoCommerce.Storefront.Controllers
     [StorefrontRoute("account")]
     public class AccountController : StorefrontControllerBase
     {
+        private readonly IStorefrontUrlBuilder _urlBuilder;
         private readonly SignInManager<User> _signInManager;
         private readonly IEventPublisher _publisher;
         private readonly StorefrontOptions _options;
@@ -44,6 +46,7 @@ namespace VirtoCommerce.Storefront.Controllers
             IOptions<StorefrontOptions> options)
             : base(workContextAccessor, urlBuilder)
         {
+            _urlBuilder = urlBuilder;
             _signInManager = signInManager;
             _publisher = publisher;
             _options = options.Value;
@@ -309,7 +312,21 @@ namespace VirtoCommerce.Storefront.Controllers
                 return View("customers/login", WorkContext);
             }
 
-            if (!new CanUserLoginToStoreSpecification(user).IsSatisfiedBy(WorkContext.CurrentStore) || new IsUserSuspendedSpecification().IsSatisfiedBy(user))
+            if (!new CanUserLoginToStoreSpecification(user).IsSatisfiedBy(WorkContext.CurrentStore))
+            {
+                if (login.ForceLoginToAccountStore)
+                {
+                    var store = WorkContext.AllStores.First(x => x.Id == user.StoreId);
+                    var url = HttpContext.Request.GetEncodedUrl();
+                    var redirectUrl = _urlBuilder.ToStoreAbsolute(url, store, store.DefaultLanguage);
+                    return RedirectPreserveMethod(redirectUrl);
+                }
+
+                WorkContext.Form.Errors.Add(SecurityErrorDescriber.UserCannotLoginInStore());
+                return View("customers/login", WorkContext);
+            }
+
+            if (new IsUserSuspendedSpecification().IsSatisfiedBy(user))
             {
                 WorkContext.Form.Errors.Add(SecurityErrorDescriber.UserCannotLoginInStore());
                 return View("customers/login", WorkContext);

VirtoCommerce.Storefront.Model/Common/IStorefrontUrlBuilder.cs
@@ -6,6 +6,7 @@ namespace VirtoCommerce.Storefront.Model.Common
     {
         string ToAppAbsolute(string virtualPath);
         string ToAppAbsolute(string virtualPath, Store store, Language language);
+        string ToStoreAbsolute(string virtualPath, Store store = null, Language language = null);
         string ToAppRelative(string virtualPath);
         string ToAppRelative(string virtualPath, Store store, Language language);
         string ToLocalPath(string virtualPath);

VirtoCommerce.Storefront.Model/Security/Login.cs
@@ -20,5 +20,7 @@ namespace VirtoCommerce.Storefront.Model.Security
         public bool RememberMe { get; set; }
 
         public bool DisplayCaptcha { get; set; }
+
+        public bool ForceLoginToAccountStore { get; set; }
     }
 }

VirtoCommerce.Storefront/Domain/Security/CustomCookieAuthenticationEvents.cs
@@ -1,28 +1,19 @@
-using System;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using System.Web;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Http;
 using VirtoCommerce.Storefront.Extensions;
-using VirtoCommerce.Storefront.Model;
 using VirtoCommerce.Storefront.Model.Common;
 
 namespace VirtoCommerce.Storefront.Domain.Security
 {
     public class CustomCookieAuthenticationEvents : CookieAuthenticationEvents
     {
-        private static readonly string[] _urlContainingQueryParameters = new string[] { "ReturnUrl", };
-
         private readonly IStorefrontUrlBuilder _storefrontUrlBuilder;
-        private readonly IWorkContextAccessor _workContextAccessor;
 
-        public CustomCookieAuthenticationEvents(IStorefrontUrlBuilder storefrontUrlBuilder, IWorkContextAccessor workContextAccessor)
+        public CustomCookieAuthenticationEvents(IStorefrontUrlBuilder storefrontUrlBuilder)
         {
             _storefrontUrlBuilder = storefrontUrlBuilder;
-            _workContextAccessor = workContextAccessor;
         }
 
         public override Task RedirectToLogin(RedirectContext<CookieAuthenticationOptions> context)
@@ -33,7 +24,7 @@ namespace VirtoCommerce.Storefront.Domain.Security
                 return Task.CompletedTask;
             }
 
-            context.RedirectUri = GetStoreAbsoluteUri(context.RedirectUri);
+            context.RedirectUri = _storefrontUrlBuilder.ToStoreAbsolute(context.RedirectUri).ToAbsolutePath();
 
             return base.RedirectToLogin(context);
         }
@@ -46,61 +37,9 @@ namespace VirtoCommerce.Storefront.Domain.Security
                 return Task.CompletedTask;
             }
 
-            context.RedirectUri = GetStoreAbsoluteUri(context.RedirectUri);
+            context.RedirectUri = _storefrontUrlBuilder.ToStoreAbsolute(context.RedirectUri).ToAbsolutePath();
 
             return base.RedirectToAccessDenied(context);
-        }
-
-
-        private string GetStoreAbsoluteUri(string uri)
-        {
-            //Need to build from an host absolute url a  relative  store-based url
-            // http://localhost/Account/Login -> http://localhost/{store}/{lang}/Account/Login
-
-            // Need to be able to properly handle the following case:
-            // http://localhost/store/Account/Login?ReturnUrl=%2Fstore%2FElectronics%2Fen-US%2Faccount, storeUrl = "http://localhost/store"
-            // 1. Should trim store path "/store" from the url path "/store/Account/Login" => path should become "/Account/Login"
-            // 2. Check for url params (e.g. ReturnUrl) in query string and trim store url for them too. ReturnUrl=%2Fstore%2FElectronics%2Fen-US%2Faccount => ReturnUrl=%2FElectronics%2Fen-US%2Faccount
-
-            var redirectUri = new UriBuilder(uri);
-            var pathWithTrimmedStorePath = new PathString(redirectUri.Path).TrimStorePath(_workContextAccessor?.WorkContext?.CurrentStore);
-            var storeBasedRedirectPath = _storefrontUrlBuilder.ToAppAbsolute(pathWithTrimmedStorePath);
-
-            ConvertParamsUrlsToStoreRelative(redirectUri);
-
-            // Checks whether path is absolute path (starts with scheme), and extract local path if it is
-            if (Uri.TryCreate(storeBasedRedirectPath, UriKind.Absolute, out var absoluteUri))
-            {
-                storeBasedRedirectPath = absoluteUri.AbsolutePath;
-            }
-
-            redirectUri.Path = storeBasedRedirectPath;
-
-            return redirectUri.Uri.ToString();
-        }
-
-        /// <summary>
-        /// Trims store path for known url containing params. Encoding/Decoding is handled by HttpUtility.ParseQueryString.
-        /// </summary>
-        /// <param name="redirectUri">Uri which query params need to be converted.</param>
-        private void ConvertParamsUrlsToStoreRelative(UriBuilder redirectUri)
-        {
-            var queryParams = HttpUtility.ParseQueryString(redirectUri.Query);
-            var allParamKeys = queryParams.AllKeys;
-
-            foreach (var paramName in _urlContainingQueryParameters)
-            {
-                var paramKey = allParamKeys.FirstOrDefault(x => x.EqualsInvariant(paramName));
-                var paramValue = paramKey != null ? queryParams[paramKey] : null;
-
-                // Need to check that param value is a valid relative url to avoid exception at PathString creation 
-                if (paramKey != null && Uri.TryCreate(paramValue, UriKind.Relative, out var _))
-                {
-                    queryParams[paramKey] = new PathString(paramValue).TrimStorePath(_workContextAccessor?.WorkContext?.CurrentStore);
-                }
-            }
-
-            redirectUri.Query = queryParams.ToString();
         }
     }
 }

VirtoCommerce.Storefront/Domain/Security/SecurityWorkContextBuilderExtensions.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using VirtoCommerce.Storefront.Model;
 using VirtoCommerce.Storefront.Model.Security;
+using VirtoCommerce.Storefront.Model.Security.Specifications;
 
 namespace VirtoCommerce.Storefront.Domain.Security
 {
@@ -26,7 +27,19 @@ namespace VirtoCommerce.Storefront.Domain.Security
             }).ToList();
 
             var user = await signInManager.UserManager.GetUserAsync(builder.HttpContext.User);
-           
+
+            if (user != null && !new CanUserLoginToStoreSpecification(user).IsSatisfiedBy(builder.WorkContext.CurrentStore))
+            {
+                await signInManager.SignOutAsync();
+                user = null;
+            }
+
+            if (user != null && new IsUserSuspendedSpecification().IsSatisfiedBy(user))
+            {
+                await signInManager.SignOutAsync();
+                user = null;
+            }
+
             //Login as a new anonymous user
             if (user == null || user.IsTransient())
             {

VirtoCommerce.Storefront/Extensions/PathStringExtensions.cs
@@ -91,5 +91,16 @@ namespace VirtoCommerce.Storefront.Extensions
 
             return path;
         }
+
+        public static PathString ToAbsolutePath(this string path)
+        {
+            // Checks whether path is absolute path (starts with scheme), and extract local path if it is
+            if (Uri.TryCreate(path, UriKind.Absolute, out var absoluteUri))
+            {
+                return absoluteUri.AbsolutePath;
+            }
+
+            return path;
+        }
     }
 }

VirtoCommerce.Storefront/Infrastructure/StorefrontUrlBuilder.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+using System.Web;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using VirtoCommerce.Storefront.Domain;
@@ -18,6 +21,9 @@ namespace VirtoCommerce.Storefront.Infrastructure
         private readonly IWorkContextAccessor _workContextAccessor;
         private readonly IHostingEnvironment _hostEnv;
         private readonly IHttpContextAccessor _httpContextAccessor;
+
+        private static readonly string[] UrlContainingQueryParameters = { "ReturnUrl", };
+
         public StorefrontUrlBuilder(IUrlBuilder urlBuilder, IWorkContextAccessor workContextAccessor, IHostingEnvironment hostEnv, IHttpContextAccessor httpContextAccessor)
         {
             _urlBuilder = urlBuilder;
@@ -43,6 +49,62 @@ namespace VirtoCommerce.Storefront.Infrastructure
                 ? _httpContextAccessor.HttpContext.Request.PathBase + appRelativePath.Replace("~", string.Empty)
                 : appRelativePath;
             return result;
+        }
+
+        public string ToStoreAbsolute(string url, Store store = null, Language language = null)
+        {
+            // Need to build from an host absolute url a  relative  store-based url
+            // http://localhost/Account/Login -> http://localhost/{store}/{lang}/Account/Login
+
+            // Need to be able to properly handle the following case:
+            // http://localhost/store/Account/Login?ReturnUrl=%2Fstore%2FElectronics%2Fen-US%2Faccount, storeUrl = "http://localhost/store"
+            // 1. Should trim store path "/store" from the url path "/store/Account/Login" => path should become "/Account/Login"
+            // 2. Check for url params (e.g. ReturnUrl) in query string and trim store url for them too. ReturnUrl=%2Fstore%2FElectronics%2Fen-US%2Faccount => ReturnUrl=%2FElectronics%2Fen-US%2Faccount
+
+            var uri = new Uri(url, UriKind.RelativeOrAbsolute);
+            var absolutePathOrUrl = ConvertPathToStoreAbsolutePathOrUrl(uri.IsAbsoluteUri ? uri.AbsolutePath : url, store, language);
+            var storeRelativeOrAbsoluteUrl = new Uri(absolutePathOrUrl, UriKind.RelativeOrAbsolute);
+            var urlBuilder = new UriBuilder(new Uri(uri, storeRelativeOrAbsoluteUrl))
+            {
+                Query = ConvertQueryUrlsToStoreAbsolutePaths(uri.Query, store, language)
+            };
+            return urlBuilder.Uri.ToString();
+        }
+
+        private string ConvertPathToStoreAbsolutePathOrUrl(PathString path, Store store = null, Language language = null)
+        {
+            var relativeToAppPath = path.TrimStorePath(_workContextAccessor.WorkContext.CurrentStore);
+            var storeUrl = store != null || language != null
+                ? ToAppAbsolute(relativeToAppPath.TrimStoreAndLangSegment(_workContextAccessor.WorkContext.CurrentStore, _workContextAccessor.WorkContext.CurrentLanguage),
+                    store ?? _workContextAccessor.WorkContext.CurrentStore,
+                    language ?? store.DefaultLanguage ?? _workContextAccessor.WorkContext.CurrentLanguage)
+                : ToAppAbsolute(relativeToAppPath);
+
+            return storeUrl;
+        }
+
+        /// <summary>
+        /// Trims store path for known url containing params. Encoding/Decoding is handled by HttpUtility.ParseQueryString.
+        /// </summary>
+        /// <param name="query">Query params need to be converted.</param>
+        private string ConvertQueryUrlsToStoreAbsolutePaths(string query, Store store = null, Language language = null)
+        {
+            var queryParams = HttpUtility.ParseQueryString(query);
+            var allParamKeys = queryParams.AllKeys;
+
+            foreach (var paramName in UrlContainingQueryParameters)
+            {
+                var paramKey = allParamKeys.FirstOrDefault(x => x.EqualsInvariant(paramName));
+                var paramValue = paramKey != null ? queryParams[paramKey] : null;
+
+                // Need to check that param value is a valid relative url to avoid exception at PathString creation 
+                if (paramKey != null && Uri.TryCreate(paramValue, UriKind.Relative, out _))
+                {
+                    queryParams[paramKey] = ConvertPathToStoreAbsolutePathOrUrl(new PathString(paramValue), store, language).ToAbsolutePath();
+                }
+            }
+
+            return queryParams.ToString();
         }
 
         public string ToAppRelative(string virtualPath)

commit f24807ed12a817d0e8d1ac7b41ea7d6f8431e79f

VP-2593: Fix login errors appearance
Fixed regression cause by https://github.com/VirtoCommerce/vc-storefront-core/pull/409

VirtoCommerce.Storefront.Model/Security/Specifications/IsUserLockedOutSpecification.cs
@@ -1,0 +1,12 @@
+using VirtoCommerce.Storefront.Model.Common.Specifications;
+
+namespace VirtoCommerce.Storefront.Model.Security.Specifications
+{
+    public class IsUserLockedOutSpecification : ISpecification<User>
+    {
+        public virtual bool IsSatisfiedBy(User obj)
+        {
+            return obj.IsLockedOut;
+        }
+    }
+}

VirtoCommerce.Storefront.Model/Security/Specifications/IsUserSuspendedSpecification.cs
@@ -6,7 +6,7 @@ namespace VirtoCommerce.Storefront.Model.Security.Specifications
     {
         public bool IsSatisfiedBy(User obj)
         {
-            return obj.UserState == AccountState.Rejected || obj.IsLockedOut;
+            return obj.UserState == AccountState.Rejected;
         }
     }
 }

VirtoCommerce.Storefront/Controllers/AccountController.cs
@@ -326,9 +326,14 @@ namespace VirtoCommerce.Storefront.Controllers
                 return View("customers/login", WorkContext);
             }
 
+            if (new IsUserLockedOutSpecification().IsSatisfiedBy(user))
+            {
+                return View("lockedout", WorkContext);
+            }
+
             if (new IsUserSuspendedSpecification().IsSatisfiedBy(user))
             {
-                WorkContext.Form.Errors.Add(SecurityErrorDescriber.UserCannotLoginInStore());
+                WorkContext.Form.Errors.Add(SecurityErrorDescriber.AccountIsBlocked());
                 return View("customers/login", WorkContext);
             }
 