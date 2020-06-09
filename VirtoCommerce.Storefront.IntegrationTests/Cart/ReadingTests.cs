1faabc03844a19ac399327676fdafe80c8c37653
Yan Brovkin <brovkin.yan@gmail.com>
2020-06-09 10:46:44 +0200

    feat #VP-2094: adding integration tests template

---
+++ VirtoCommerce.Storefront.IntegrationTests/Cart/ReadingTests.cs
@@ -0,0 +1,34 @@
+using Xunit;
+
+namespace VirtoCommerce.Storefront.IntegrationTests.Cart
+{
+
+    public class ReadingTests
+    {
+        public ReadingTests()
+        {
+        }
+
+        [Fact]
+        public void GetCart_IfCartIsNotExistForUser_ShouldReturnNewCart()
+        {
+        }
+
+        [Fact]
+        public void GetCart_IfCartExistForUser_ShouldReturnExistedCart()
+        {
+        }
+
+        [Fact]
+        public void GetCartItemsCount_IfCartHasNoItems_ShouldReturnZero()
+        {
+        }
+
+        [Fact]
+        public void GetCartItemsCount_IfCartHasItems_ShouldReturnExactItemsCount()
+        {
+        }
+
+
+    }
+}
---
+++ VirtoCommerce.Storefront.IntegrationTests/Cart/WritingTests.cs
@@ -0,0 +1,26 @@
+using Xunit;
+
+namespace VirtoCommerce.Storefront.IntegrationTests.Cart
+{
+    public class WritingTests
+    {
+        public WritingTests()
+        {
+        }
+
+        [Fact]
+        public void ClearCart_IfCartExists_ShouldReturnEmptyCart()
+        {
+        }
+
+        [Fact]
+        public void ClearCart_IfAddedItemAfterClear_ShouldReturnCartWithThisItem()
+        {
+        }
+
+        [Fact]
+        public void ClearCart_IfCartNotExist_ShouldReturnBadRequest()
+        {
+        }
+    }
+}
