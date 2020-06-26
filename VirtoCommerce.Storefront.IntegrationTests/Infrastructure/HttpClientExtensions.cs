312eb31434430aaa68329939f57869f99de8f466
Evan Butler <evan.butler@icloud.com>
2020-06-26 23:02:02 +0700

    vp-2443 cart api integration tests (#448)
    * feat #vp-2443: add infrastructure for integration tests of cart api
    
    * fix: ComparingByMembers<ShoppingCart>() in tests instead of ComparingByValue
    
    * feat: scaffolding of integration infrastructure
    
    * feat: finished scaffolding & some tests of cart
    
    * feat: dispose HttpClient inside ApiCartControllerTests.Dispose instead of call using inside each test case
    
    * chore: some beautifying code inside integration tests
    
    * fix: integration tests
    
    * fix: integration tests
    
    * feat: add some more tests of cart modification, payment methods
    
    * feat (XPurchase): add more integration tests & improve test infrastructure
    
    * Feature: add some integration tests for coupons, payment & shipment plans
    
    * feat: implement api cart controller tests
    
    Co-authored-by: Yan Brovkin <brovkin.yan@gmail.com>

--- VirtoCommerce.Storefront.IntegrationTests/Infrastructure/HttpClientExtensions.cs
+++ VirtoCommerce.Storefront.IntegrationTests/Infrastructure/HttpClientExtensions.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using VirtoCommerce.Storefront.IntegrationTests.Models;
 using VirtoCommerce.Storefront.Model.Cart;
+using VirtoCommerce.Storefront.Model.Subscriptions;
 
 namespace VirtoCommerce.Storefront.IntegrationTests.Infrastructure
 {
@@ -29,7 +30,7 @@ namespace VirtoCommerce.Storefront.IntegrationTests.Infrastructure
 
         public static HttpClient Login(this HttpClient client, string userName, string password)
         {
-            var loginPage = client.GetAsync("account/login").GetAwaiter().GetResult();
+            var loginPage = client.GetAsync(TestEnvironment.LoginEndpoint).GetAwaiter().GetResult();
 
             var antiforgery = new AntiforgeryCookie(loginPage.Headers);
             var content = new MultipartFormDataContent
@@ -46,9 +47,20 @@ namespace VirtoCommerce.Storefront.IntegrationTests.Infrastructure
             }
 
             var cookie = new AntiforgeryCookie(loginResponse.Headers);
-            client.DefaultRequestHeaders.Add(cookie.Key, new[] { cookie.Value });
+            return SetAntiforgeryHeader(client, cookie);
+        }
 
-            return client;
+        public static HttpClient GotoMainPage(this HttpClient client)
+        {
+            var mainPage = client.GetAsync("").GetAwaiter().GetResult();
+            if (!mainPage.IsSuccessStatusCode)
+            {
+                throw new Exception($"Main page loading failed: {mainPage.StatusCode}");
+            }
+
+            var antiforgery = new AntiforgeryCookie(mainPage.Headers);
+
+            return SetAntiforgeryHeader(client, antiforgery);
         }
 
         public static HttpClient InsertCartItem(this HttpClient client, AddCartItem item)
@@ -67,12 +79,50 @@ namespace VirtoCommerce.Storefront.IntegrationTests.Infrastructure
             return client;
         }
 
-        public static HttpClient СlearCart(this HttpClient client)
+        public static HttpClient ChangeCartItemPrice(this HttpClient client, ChangeCartItemPrice priceItem)
+        {
+            var content = new StringContent(
+                JsonConvert.SerializeObject(priceItem),
+                Encoding.UTF8,
+                "application/json");
+
+            var response = client.PutAsync(TestEnvironment.ItemPriceEndpoint, content).GetAwaiter().GetResult();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"ChangeCartItemPrice failed: {response.StatusCode}");
+            }
+
+            return client;
+        }
+
+        public static HttpClient ClearCart(this HttpClient client)
         {
             var response = client.PostAsync(TestEnvironment.CartClearEndpoint, new StringContent("")).GetAwaiter().GetResult();
             if (!response.IsSuccessStatusCode)
             {
                 throw new Exception($"Clear cart failed: {response.StatusCode}");
+            }
+
+            return client;
+        }
+
+        public static HttpClient AddCoupon(this HttpClient client, string couponCode)
+        {
+            var response = client.PostAsync(TestEnvironment.AddCouponEndpoint(couponCode), null).GetAwaiter().GetResult();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Add coupon failed: {response.StatusCode}");
+            }
+
+            return client;
+        }
+
+        public static HttpClient RemoveCoupon(this HttpClient client, string couponCode)
+        {
+            var response = client.DeleteAsync(TestEnvironment.RemoveCouponEndpoint(couponCode)).GetAwaiter().GetResult();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Remove coupon failed: {response.StatusCode}");
             }
 
             return client;
@@ -85,6 +135,113 @@ namespace VirtoCommerce.Storefront.IntegrationTests.Infrastructure
             {
                 throw new Exception($"Logout failed: {response.StatusCode}");
             }
+
+            if (client.DefaultRequestHeaders.TryGetValues(AntiforgeryCookie.Name, out var tokens))
+            {
+                client.DefaultRequestHeaders.Remove(AntiforgeryCookie.Name);
+            }
+
+            return client;
+        }
+
+        public static HttpClient AddOrUpdateCartPaymentPlan(this HttpClient client, PaymentPlan paymentPlan)
+        {
+            var content = new StringContent(
+                JsonConvert.SerializeObject(paymentPlan),
+                Encoding.UTF8,
+                "application/json");
+
+            var response = client.PostAsync(TestEnvironment.CartPaymentPlanEndpoint, content).GetAwaiter().GetResult();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Add or update cart payment plan failed: {response.StatusCode}");
+            }
+
+            return client;
+        }
+
+        public static HttpClient DeleteCartPaymentPlan(this HttpClient client)
+        {
+            var response = client.DeleteAsync(TestEnvironment.CartPaymentPlanEndpoint).GetAwaiter().GetResult();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Delete cart payment plan failed: {response.StatusCode}");
+            }
+
+            return client;
+        }
+
+        public static async Task<HttpResponseMessage> AddOrUpdateCartPayment(this HttpClient client, Payment payment)
+        {
+            var content = new StringContent(
+                JsonConvert.SerializeObject(payment),
+                Encoding.UTF8,
+                "application/json");
+
+            var response = await client.PostAsync(TestEnvironment.CartPaymentEndpoint, content);
+
+            return response;
+        }
+
+        public static async Task<HttpResponseMessage> AddOrUpdateCartShipment(this HttpClient client, Shipment shipment)
+        {
+            var content = new StringContent(
+                JsonConvert.SerializeObject(shipment),
+                Encoding.UTF8,
+                "application/json");
+
+            var response = await client.PostAsync(TestEnvironment.CartShipmentEndpoint, content);
+
+            return response;
+        }
+
+        public static async Task<string> GetCart(this HttpClient client)
+        {
+            var response = await client.GetAsync(TestEnvironment.CartEndpoint);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Get cart failed: {response.StatusCode}");
+            }
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public static async Task<string> GetCartAvailPaymentMethods(this HttpClient client)
+        {
+            var response = await client.GetAsync(TestEnvironment.PaymentMethodsEndpoint);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Get cart available payment methods failed: {response.StatusCode}");
+            }
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public static async Task<string> GetCartShipmentAvailShippingMethods(this HttpClient client)
+        {
+            var response = await client.GetAsync(TestEnvironment.ShippingMethodsEndpoint("1"));
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Get cart available shipping methods failed: {response.StatusCode}");
+            }
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+
+        private static HttpClient SetAntiforgeryHeader(HttpClient client, AntiforgeryCookie cookie)
+        {
+            if (client.DefaultRequestHeaders.TryGetValues(AntiforgeryCookie.Name, out var tokens))
+            {
+                client.DefaultRequestHeaders.Remove(AntiforgeryCookie.Name);
+            }
+
+            client.DefaultRequestHeaders.Add(AntiforgeryCookie.Name, new[] { cookie.Value });
 
             return client;
         }
--- VirtoCommerce.Storefront.IntegrationTests/Infrastructure/TestEnvironment.cs
+++ VirtoCommerce.Storefront.IntegrationTests/Infrastructure/TestEnvironment.cs
@@ -8,5 +8,24 @@ namespace VirtoCommerce.Storefront.IntegrationTests.Infrastructure
         public static string CartClearEndpoint => "storefrontapi/cart/clear";
         public static string LoginEndpoint => "account/login";
         public static string LogoutEndpoint => "account/logout";
+        public static string ItemPriceEndpoint => "storefrontapi/cart/items/price";
+        public static string PaymentMethodsEndpoint => "storefrontapi/cart/paymentmethods";
+        public static string CartPaymentPlanEndpoint => "storefrontapi/cart/paymentPlan";
+        public static string CartPaymentEndpoint => "storefrontapi/cart/payments";
+        public static string CartShipmentEndpoint => "storefrontapi/cart/shipments";
+
+        public static string AddCouponEndpoint(string couponCode) =>
+            $"storefrontapi/cart/coupons/{couponCode}";
+        public static string RemoveCouponEndpoint(string couponCode) =>
+            $"storefrontapi/cart/coupons{(string.IsNullOrWhiteSpace(couponCode) ? "" : $"?couponCode={couponCode}")}";
+        public static string ShippingMethodsEndpoint(string shippmentId) =>
+            $"storefrontapi/cart/shipments/{shippmentId}/shippingmethods";
+
+    }
+
+    public static class Product
+    {
+        public static string Octocopter => "e7eee66223da43109502891b54bc33d3";
+        public static string Quadcopter => "9cbd8f316e254a679ba34a900fccb076";
     }
 }
