d58246a3b7a1b01c242730aa89e38a8cd04ac9d0
Evan Butler <evan.butler@icloud.com>
2020-06-30 13:02:33 +0700

    VP-2443 implement lists api integration tests (#451)
    * feat #vp-2443: add infrastructure for integration tests of cart api
    
    * fix: ComparingByMembers<ShoppingCart>() in tests instead of ComparingByValue
    
    * feat: scaffolding of integration infrastructure
    
    * feat: finished scaffolding & some tests of cart
    
    * feat: dispose HttpClient inside ApiCartControllerTests.Dispose instead of call using inside each test case
    
    * chore: some beautifying code inside integration tests
    
    * fix: integration tests
    
    * fix: integration tests
    
    * feat: add some more tests of cart modification, payment methods
    
    * feat (XPurchase): add more integration tests & improve test infrastructure
    
    * Feature: add some integration tests for coupons, payment & shipment plans
    
    * feat: implement api cart controller tests
    
    * feat: Add infastucture for list integration tests
    
    Co-authored-by: Yan Brovkin <brovkin.yan@gmail.com>

---
+++ VirtoCommerce.Storefront.IntegrationTests/Tests/ApiListsControllerTests.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using VirtoCommerce.Storefront.IntegrationTests.Infrastructure;
+using Xunit;
+
+namespace VirtoCommerce.Storefront.IntegrationTests.Tests
+{
+    [Trait("Category", "CI")]
+    [CollectionDefinition("ApiCartControllerTests", DisableParallelization = true)]
+    public class ApiListsControllerTests : IClassFixture<StorefrontApplicationFactory>, IDisposable
+    {
+        private readonly HttpClient _client;
+        private bool _isDisposed;
+
+        public ApiListsControllerTests(StorefrontApplicationFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                _client?.Dispose();
+            }
+
+            _isDisposed = true;
+        }
+
+        ~ApiListsControllerTests()
+        {
+            Dispose(false);
+        }
+
+        [Fact]
+        public async Task GetListByName_IfListWithNameExist_ShouldReturnExistedList()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task GetListByName_IfListWithNameIsNotExist_ShouldReturnEmptyList()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task GetListsWithProduct_IfListWithProductExist_ShouldReturnExistedList()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task GetListsWithProduct_IfListWithProductIsNotExist_ShouldReturnEmptyList()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task GetListItemsCount_IfListHasNoItems_ShouldReturnZero()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task GetListItemsCount_IfListHasItems_ShouldReturnExactItemsCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task AddItemToList_IfItemIsAvailable_ShouldReturnListItemsCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task AddItemToList_IfItemIsNotAvailable_ShouldNotUpdateListItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task RemoveItemFromList_IfItemAvailable_ShouldReturnListItemsCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task ClearList_IfListExists_ShouldReturnEmptyList()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task SearchList_IfListExists_ShouldReturnList()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task SearchList_IfListNotExists_ShouldReturnEmptyResponse()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task CreateList_ShouldCreateListFromCart()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task MergeListWithCurrentCart_ShouldReturnMergedCart()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
