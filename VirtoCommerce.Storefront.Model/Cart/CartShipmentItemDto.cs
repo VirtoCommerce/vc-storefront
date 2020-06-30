7fde09980d3f8ae5a6141ccca422b872d12cd6ae
Yan Brovkin <brovkin.yan@gmail.com>
2020-06-30 11:54:31 +0200

    refactor: remove unused models

--- VirtoCommerce.Storefront.Model/Cart/CartShipmentItemDto.cs
+++
@@ -1,8 +0,0 @@
-namespace VirtoCommerce.Storefront.Model.Cart
-{
-    public class CartShipmentItemDto
-    {
-        public int Quantity { get; set; }
-        public LineItemDto LineItem { get; set; }
-    }
-}
