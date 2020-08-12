d098784d3c4379144c5ce186aedab6dd59e57d65
Aleksandr Andreev <alivemenone@gmail.com>
2020-08-12 16:32:48 +0300

    Remove unused constructor

--- VirtoCommerce.Storefront.Model/Cart/ValidationErrors/ValidationError.cs
+++ VirtoCommerce.Storefront.Model/Cart/ValidationErrors/ValidationError.cs
@@ -4,11 +4,6 @@ namespace VirtoCommerce.Storefront.Model.Cart.ValidationErrors
 {
     public abstract class ValidationError : ValueObject
     {
-        protected ValidationError()
-        {
-         
-        }
-
         public string ErrorCode { get; set; }
         public string ObjectType { get; set; }
         public string ObjectId { get; set; }
