fc2b987ca98f14122607bb75572c2554c8a5ba96
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2020-08-19 16:45:46 +0200

    Move term converter and change escaping rules

--- VirtoCommerce.Storefront.Model/Catalog/TermExtensions.cs
+++ VirtoCommerce.Storefront.Model/Catalog/TermExtensions.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
+using VirtoCommerce.Storefront.Model.Common;
 
 namespace VirtoCommerce.Storefront.Model.Catalog
 {
@@ -30,27 +32,84 @@ namespace VirtoCommerce.Storefront.Model.Catalog
             List<string> result = null;
 
             const string commaEscapeString = "%x2C";
-            const string spaceEscapeString = "%x20";
 
             if (terms != null)
             {
+                var rangePattern = new Regex(@"^(\[|\(){1}.*(\]|\)){1}$");
+
                 var strings = terms
                     .GroupBy(t => t.Name, t => t, StringComparer.OrdinalIgnoreCase)
                     .OrderBy(g => g.Key)
                     .Select(
                         g =>
                         // VP-3940: Need to encode spaces to allow search engine treat them differently - as delimiters and as pure spaces in name/values
-                            string.Join(":", encode ? g.Key?.Replace(" ", spaceEscapeString) : g.Key,
+                            string.Join(":", encode && g.Key.Contains(" ") ? $@"\""{g.Key}\""" : g.Key,
                                 string.Join(",",
-                                    g.Select(t => encode ? t.Value?.Replace(",", commaEscapeString)?.Replace(" ", spaceEscapeString) : t.Value)
+                                        g.Select(t =>
+                                            {
+                                                string termValue;
+                                                if (encode)
+                                                {
+                                                    termValue = t.Value.Replace(",", commaEscapeString);
+
+                                                    if (termValue.Contains(" ") && !rangePattern.IsMatch(termValue))
+                                                    {
+                                                        termValue = $@"\""{termValue}\""";
+                                                    }
+                                                }
+                                                else
+                                                {
+                                                    termValue = t.Value;
+                                                }
+
+                                                return termValue;
+                                            })
                                         .Distinct(StringComparer.OrdinalIgnoreCase)
-                                        .OrderBy(v => v))))
+                                        .OrderBy(v => v)
+                                    )
+                                )
+                        )
+
                     .ToList();
 
                 if (strings.Any())
                 {
                     result = strings;
                 }
+            }
+
+            return result;
+        }
+
+        public static void ConvertTerm(this Term term, string currencyCode)
+        {
+            var values = term.Value.Split('-');
+
+            var templateArgument = values.FirstOrDefault();
+            var isLowerLimitInteger = int.TryParse(templateArgument, out _);
+            int.TryParse(values.Skip(1).FirstOrDefault(), out var upperLimit);
+
+            var format = GetPriceFilterValuePattern(isLowerLimitInteger, templateArgument);
+
+            term.Value = string.Format(format, upperLimit);
+            term.Name = $"{term.Name}_{currencyCode.ToLowerInvariant()}";
+        }
+
+        private static string GetPriceFilterValuePattern(bool isInt, string limit)
+        {
+            string result = null;
+
+            if (isInt)
+            {
+                result = $@"[{limit} TO {{0}})";
+            }
+            else if (limit.EqualsInvariant("under"))
+            {
+                result = "[ TO {0})";
+            }
+            else if (limit.EqualsInvariant("over"))
+            {
+                result = "[{0} TO )";
             }
 
             return result;
