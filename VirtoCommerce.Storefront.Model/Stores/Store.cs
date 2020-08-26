48829c7c44f52e97ffebb05fb16940177426d63e
trueboroda <superiorboroda@gmail.com>
2020-08-26 18:31:14 +0300

    VP-4415: Add additional checking of scheme into places with Uri.TryCreate()
    On a Unix system Uri.TryCreate() behavior is not same as on windows. It may cause problems. Therefore we need to check that scheme is not 'file' at absolute URI creating. 
    https://github.com/dotnet/runtime/issues/22718

--- VirtoCommerce.Storefront.Model/Stores/Store.cs
+++ VirtoCommerce.Storefront.Model/Stores/Store.cs
@@ -9,6 +9,8 @@ namespace VirtoCommerce.Storefront.Model.Stores
     /// </summary>
     public partial class Store : Entity, IHasSettings
     {
+        const string FILE_SCHEME = "file";
+
         public Store()
         {
             Languages = new List<Language>();
@@ -37,7 +39,7 @@ namespace VirtoCommerce.Storefront.Model.Stores
             get
             {
                 string result = null;
-                if (!string.IsNullOrEmpty(Url) && Uri.TryCreate(Url, UriKind.Absolute, out var url))
+                if (!string.IsNullOrEmpty(Url) && (Uri.TryCreate(Url, UriKind.Absolute, out var url) && url.Scheme != FILE_SCHEME))
                 {
                     result = url.Host;
                 }
