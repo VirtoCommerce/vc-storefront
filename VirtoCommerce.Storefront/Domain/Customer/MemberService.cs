577561a7e4a6e43ce8bb995b6273cb02cb6d5421
Aleksandr Vishniakov <av@virtoway.com>
2020-08-28 09:06:23 +0200

    Use Search Criteria

--- VirtoCommerce.Storefront/Domain/Customer/MemberService.cs
+++ VirtoCommerce.Storefront/Domain/Customer/MemberService.cs
@@ -201,13 +201,16 @@ namespace VirtoCommerce.Storefront.Domain
 
         public async Task<IPagedList<Contact>> SearchOrganizationContactsAsync(OrganizationContactsSearchCriteria criteria)
         {
-            var searchResult = await _customerApi.SearchContactsAsync(
-                memberId: criteria.OrganizationId,
-                skip: (criteria.PageNumber - 1) * criteria.PageSize,
-                take: criteria.PageSize,
-                sort: criteria.Sort,
-                searchPhrase: criteria.SearchPhrase,
-                objectType: "Member");
+            var crtiteriaDto = new customerDto.MembersSearchCriteria
+            {
+                MemberId = criteria.OrganizationId,
+                Skip = (criteria.PageNumber - 1) * criteria.PageSize,
+                Take = criteria.PageSize,
+                Sort = criteria.Sort,
+                SearchPhrase = criteria.SearchPhrase,
+                ObjectType = "Member"
+            };
+            var searchResult = await _customerApi.SearchContactsAsync(crtiteriaDto);
             var contacts = searchResult.Results.Select(x => x.ToContact()).ToList();
 
             return new StaticPagedList<Contact>(contacts, criteria.PageNumber, criteria.PageSize, searchResult.TotalCount ?? 0);
