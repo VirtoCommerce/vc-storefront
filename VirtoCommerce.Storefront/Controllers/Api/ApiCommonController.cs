7096bd6a75c3510a2a1228d8f490f01b1195ddb1
Eugeny Tatarincev <et@virtoway.com>
2020-10-14 15:48:04 +0200

    Add some missed REST API controllers

---
+++ VirtoCommerce.Storefront/Controllers/Api/ApiCommonController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VirtoCommerce.Storefront.AutoRestClients.StoreModuleApi;
+using VirtoCommerce.Storefront.Domain;
+using VirtoCommerce.Storefront.Infrastructure;
+using VirtoCommerce.Storefront.Model;
+using VirtoCommerce.Storefront.Model.Common;
+
+namespace VirtoCommerce.Storefront.Controllers.Api
+{
+    [StorefrontApiRoute("")]
+    [ResponseCache(CacheProfileName = "None")]
+    public class ApiCommonController : StorefrontControllerBase
+    {
+        private readonly IStoreModule _storeApi;
+        private readonly Country[] _countriesWithoutRegions;
+
+        public ApiCommonController(IWorkContextAccessor workContextAccessor, IStorefrontUrlBuilder urlBuilder, IStoreModule storeApi)
+            : base(workContextAccessor, urlBuilder)
+        {
+            _storeApi = storeApi;
+            _countriesWithoutRegions = WorkContext.AllCountries
+             .Select(c => new Country { Name = c.Name, Code2 = c.Code2, Code3 = c.Code3, RegionType = c.RegionType })
+             .ToArray();
+        }
+
+        // GET: storefrontapi/countries
+        [HttpGet("countries")]
+        public ActionResult<Country[]> GetCountries()
+        {
+            return _countriesWithoutRegions;
+        }
+
+        // GET: storefrontapi/countries/{countryCode}/regions
+        [HttpGet("countries/{countryCode}/regions")]
+        public ActionResult<CountryRegion[]> GetCountryRegions(string countryCode)
+        {
+            var country = WorkContext.AllCountries.FirstOrDefault(c => c.Code2.Equals(countryCode, StringComparison.InvariantCultureIgnoreCase) || c.Code3.Equals(countryCode, StringComparison.InvariantCultureIgnoreCase));
+            if (country != null)
+            {
+                return country.Regions;
+            }
+            return Ok();
+        }
+
+        // POST: storefrontapi/feedback
+        [HttpPost("feedback")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Feedback([FromBody] ContactForm model)
+        {
+            await _storeApi.SendDynamicNotificationAnStoreEmailAsync(model.ToServiceModel(WorkContext));
+
+            return Ok();
+        }
+    }
+}
