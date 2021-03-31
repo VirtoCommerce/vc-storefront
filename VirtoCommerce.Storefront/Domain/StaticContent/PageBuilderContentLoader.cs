490f4bde8fa10f28f28497296c4e682ef937deee
Sergey Podguzov <spo@virtoway.com>
2021-03-31 22:04:31 +0500

    Checking for json objects at reading metadata in static content.

--- VirtoCommerce.Storefront/Domain/StaticContent/PageBuilderContentLoader.cs
+++ VirtoCommerce.Storefront/Domain/StaticContent/PageBuilderContentLoader.cs
@@ -21,7 +21,22 @@ namespace VirtoCommerce.Storefront.Domain
 
             foreach (JProperty item in items)
             {
-                metadata.Add(item.Name, new List<string> { item.Value.Value<string>() });
+                if (item.Value.HasValues)
+                {
+                    foreach (var itemProp in item.Value)
+                    {
+                        var property = itemProp.ToObject<JProperty>();
+                        metadata.Add($"{item.Name}.{property.Name}", new List<string> { property.Value.Value<string>() });
+                    }
+                }
+                else
+                {
+                    var value = item.Value.Value<string>();
+                    if (value != null)
+                    {
+                        metadata.Add(item.Name, new List<string> { value });
+                    }
+                }
             }
 
             metadata.Add("template", new List<string> { "json-page" });
