e0006d78d7f14411b1086eb7230029fdc47be512
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2021-07-23 09:45:44 +0200

    Update VirtoCommerce.Storefront/Infrastructure/GraphQLExecutionResult.cs
    Co-authored-by: Filipp Kuksov <47894313+Vectorfield4@users.noreply.github.com>

--- VirtoCommerce.Storefront/Infrastructure/GraphQLExecutionResult.cs
+++ VirtoCommerce.Storefront/Infrastructure/GraphQLExecutionResult.cs
@@ -3,15 +3,15 @@ namespace VirtoCommerce.Storefront.Infrastructure
     public class GraphQLExecutionResult
     {
         public GraphQLError[] Errors { get; set; }
-    }
-
-    public class GraphQLError
-    {
-        public GraphQLExtensions Extensions { get; set; }
-    }
-
-    public class GraphQLExtensions
-    {
-        public string Code { get; set; }
+        
+        public class GraphQLError
+        {
+            public GraphQLExtensions Extensions { get; set; }
+            
+            public class GraphQLExtensions
+            {
+                public string Code { get; set; }
+            }
+        }
     }
 }
